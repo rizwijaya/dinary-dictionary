11d2dd195e5fa50a91d5581abc5c63d8a709961e
Rizqi Wijaya <rizwijaya@github.com>
2021-03-30 07:20:23 +0700

    Create Speech Recognitions.

--- src/components/english/input.js
+++ src/components/english/input.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
 import { Row, Col } from 'antd';
 import Search from "antd/lib/input/Search";
-
+import Speech from "./speech";
 export default function Input() {
     const[error, setError] = useState("");
     const[word, setWord] = useState("");
@@ -39,6 +39,7 @@ export default function Input() {
             <Col span={12} offset={6}>
                 <h1> Dinary - Dictionary English</h1>
                 <Search placeholder="input search text" onSearch={value => {searching(value);}} enterButton />
+                <Speech/>
             </Col>
             <Col span={6} offset={6}>
             {(() => {
