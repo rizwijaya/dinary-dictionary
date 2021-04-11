2f8cbb5092868c104f00d516f506b5125c486d51
Rizqi Wijaya <rizwijaya@github.com>
2021-04-11 11:18:15 +0700

    Add React Router Dom and Create Router Link.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-// import Input from "./components/english/input";
-import InputUK from "./components/englishUK/inputUK";
+import "./App.css";
+import English from "./components/english/input";
+import EnglishUK from "./components/englishUK/inputUK";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 function App() {
   return (
+    <Router>
     <div className="App">
-      <InputUK />
+      <h1>Dinary - Dictionary English</h1>
+      <ul>
+        <Link to="/">
+          <li>Dictionary English</li>
+        </Link>
+        <Link to="/BritainsEnglish">
+          <li>Dictionary Britains English</li>
+        </Link>
+      </ul>
+      <Switch>
+      <Route path="/" exact component={English} />
+      <Route path="/BritainsEnglish" component={EnglishUK} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
