b7121713f2361c6919667642ebef042bd0c599ad
Rizqi Wijaya <rizwijaya@github.com>
2021-04-11 22:20:20 +0700

    Create Navbar and Custom Route Links.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import React from "react";
 import "./App.css";
 import English from "./components/english/input";
 import EnglishUK from "./components/englishUK/inputUK";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <Router>
     <div className="App">
-      <h1>Dinary - Dictionary English</h1>
-      <ul>
-        <Link to="/">
-          <li>Dictionary English</li>
-        </Link>
-        <Link to="/BritainsEnglish">
-          <li>Dictionary Britains English</li>
-        </Link>
-      </ul>
-      <Switch>
-      <Route path="/" exact component={English} />
-      <Route path="/BritainsEnglish" component={EnglishUK} />
-      </Switch>
-    </div>
-    </Router>
+      <Router>
+        <h1 className="h12 mb-0">dinary</h1>
+        <Nav className="justify-content-center" activeKey="/">
+          <Nav.Item>
+            <Nav.Link><Link to='/'>English</Link></Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              |
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link><Link to='/BritainsEnglish'>Britains English</Link></Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Switch>
+          <Route path="/" exact component={English} />
+          <Route path="/BritainsEnglish" component={EnglishUK} />
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
